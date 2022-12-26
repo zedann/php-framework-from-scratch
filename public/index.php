af4fb50415bef48de795123fa26cfccfe39976d1
zedann <zedamohamed144@gmail.com>
2022-12-26 15:33:59 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+use app\core\Application;
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$app = new Application(dirname(__DIR__));
+$app->router->get('/','home');
+$app->router->get('/contact','contact');
+$app->router->post('/contact', function () {
+    return "handling submitted form";
+});
+$app->run();
